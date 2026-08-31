87a5d1f8398052bd53ee3af919159a2184870457
Gautam vemula <gautamvemula@gmail.com>
2026-08-31 17:11:23 +0530

    Add fork example in q3b.c
    Implement a simple fork example demonstrating process creation and management.

---
+++ 2520030316_Practical/Practical-03/q3b.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<sys/wait.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+int main(){
+pid_t pid;
+printf("Before fork()\n");
+printf("Current process PID : %d\n", getpid());
+pid = fork();
+ if (pid < 0)
+    {
+printf("Fork failed.\n");
+ exit(1);
+  }
+else if (pid == 0){
+printf("this is child procress\n");
+printf("Status: Running\n");
+printf("PID : %d\n", getpid());
+printf("PPID    : %d\n", getppid());
+printf("the child procress is completed");
+exit(0);
+}
+else{
+printf("Parent Process\n");
+printf("State : Running\n");
+printf("Parent PID : %d\n", getpid());
+printf("Child PID  : %d\n", pid);
+printf("Parent State : Waiting for Child\n");
+wait(NULL);
+printf("Child has terminated.\n");
+printf("Parent State : Running\n");
+printf("Parent process terminated.\n");
+    }
+
+    return 0;
+}
